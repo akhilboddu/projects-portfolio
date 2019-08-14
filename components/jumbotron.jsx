54c22c4d03ece4d463268f40910ded8db48ee1be
Akhil Boddu <akhil1234mara@gmail.com>
2019-08-15 00:54:25 +0200

    added typing effect

--- components/jumbotron.jsx
+++ components/jumbotron.jsx
@@ -1,11 +1,41 @@
 import PropTypes from 'prop-types';
+import Typist from 'react-typist';
 
-const Jumbotron = ({ phrase, subtitle }) => (
-  <div className="jumbotron text-center">
-    <h1 className="display-4 jumbotronMain"><b>{phrase}</b></h1>
-    <p className="lead">{subtitle}</p>
-  </div>
-);
+import React, { Component } from 'react';
+
+class Jumbotron extends Component {
+  state = {
+    display: '',
+    renderMsg: false,
+  }
+
+  componentWillMount() {
+    this.setState({ display: this.props.subtitle });
+  }
+
+  onHeaderTyped = () => {
+    console.log('test');
+    this.setState({ display: '' });
+  }
+
+  render() {
+    const { phrase, subtitle } = this.props;
+    return (
+      <div className="jumbotron text-center">
+        <h1 className="display-4 jumbotronMain"><b>{phrase}</b></h1>
+        <Typist
+          className="lead"
+          avgTypingDelay={40}
+          startDelay={2000}
+          onTypingDone={this.onHeaderTyped}
+        >
+          {this.state.display}
+        </Typist>
+        {/* <p className="lead"></p> */}
+      </div>
+    );
+  }
+}
 
 Jumbotron.propTypes = {
   phrase: PropTypes.string,
